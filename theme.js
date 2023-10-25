39f391e5cbb27fad6f178113ef1b24e193ab873d
Beatrice Baret <bbaret@techer.digital>
2023-10-25 15:33:45 +0200

    updateThemeBTN

--- theme.js
+++ theme.js
@@ -19,21 +19,33 @@ function setColor(color, getColor, border) {
 
     }
 }
+function setActive(bool, btn, btnNotActive) {
+    if (bool) {
+        btn.style.boxShadow = "inset 0px 0px 25px -2px rgba(0,0,0,0.38)";
+        btnNotActive.style.boxShadow = "";
+    } else {
+        btn.style.boxShadow = "";
+    }
+}
+
 
 if (!myTheme) {
     setColor("", "rgb(60, 60, 60)", "");
+    setActive(!myTheme, light, dark)
 } else {
     setColor("rgb(60, 60, 60)", "rgb(255, 255, 255)", "rgb(103, 103, 103)")
+    setActive(myTheme, dark, light)
 }
 
 light.addEventListener("click", () => {
     myTheme = false;
     setColor("", "rgb(60, 60, 60)", "");
     localStorage.setItem("myTheme", myTheme);
+    setActive(!myTheme, light, dark)
 })
 dark.addEventListener("click", () => {
     myTheme = true;
     setColor("rgb(60, 60, 60)", "rgb(255, 255, 255)", "rgb(103, 103, 103)");
     localStorage.setItem("myTheme", myTheme);
-
+    setActive(myTheme, dark, light)
 })
